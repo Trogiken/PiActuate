976eda930643d421f32b74c48bb4e50d4adc202a
Trogiken <noah@blaszak.com>
2022-04-18 12:11:11 -0400

    uplink!?

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from door import Door
+# from door import Door
+import anvil.server
 
 # Set static IP for RPI
 # Check for update button that pulls from GitHub
 # Change max travel time
 
-door = Door()
+# use anvil to make app, publish it to an embed
 
-door.move('up')
+# door = Door()
+#
+# door.move('up')
+#
+# door.maximum_travel_time = 10
 
-door.maximum_travel_time = 10
+anvil.server.connect("V5QNUE3PMZD42P7RSPOVDGL5-PTAOXCGWB7VCBPZK")
+
+
+@anvil.server.callable
+def show_message(message):
+    print(message)
+
+
+anvil.server.wait_forever()
