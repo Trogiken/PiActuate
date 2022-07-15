d0e65ea577b5a5fdde7e2c283d7826630410731a
Trogiken <noah@blaszak.com>
2022-07-15 15:12:34 -0400

    Door logic overhaul

--- source/door.py
+++ source/door.py
@@ -1,113 +1,74 @@
 from .base_logger import log
-import RPi.GPIO as GPIO
+import RPi.logger as GPIO
 import time
 
 
-class Door:  # MAKE SURE PINS LOAD IN AS INTEGERS INSTEAD OF STRINGS
-    def __init__(self, relay1, relay2, top_switch, bottom_switch, sensor, max_travel_time):
+class Door:
+    def __init__(self, relay1, relay2, sw1, sw2, sw3, sw4, sw5, max_travel):
         GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
         self.RELAY1 = relay1
         self.RELAY2 = relay2
-        self.TOP_SWITCH = top_switch  # Change to correct pin
-        self.BOTTOM_SWITCH = bottom_switch  # Change to correct pin
-        self.SENSOR = sensor  # Change to correct pin
-        self.max_travel_time = max_travel_time
+        self.SW1 = sw1
+        self.SW2 = sw2
+        self.SW3 = sw3
+        self.SW4 = sw4
+        self.SW5 = sw5
+        self.max_travel = max_travel
+        self.status = None
+        self.motion = 0
+
+        log.debug(f"RELAY1: {self.RELAY1}")
+        log.debug(f"RELAY2: {self.RELAY2}")
+        log.debug(f"SW1: {self.SW1}")
+        log.debug(f"SW2: {self.SW2}")
+        log.debug(f"SW3: {self.SW3}")
+        log.debug(f"SW4: {self.SW4}")
+        log.debug(f"SW5: {self.SW5}")
+        log.debug(f"max_travel: {self.max_travel}")
+
         GPIO.setup(self.RELAY1, GPIO.OUT, initial=True)
         GPIO.setup(self.RELAY2, GPIO.OUT, initial=True)
-        GPIO.setup(self.TOP_SWITCH, GPIO.IN)
-        GPIO.setup(self.BOTTOM_SWITCH, GPIO.IN)
-        GPIO.setup(self.SENSOR, GPIO.IN)
+        GPIO.setup(self.SW1, GPIO.IN)
+        GPIO.setup(self.SW2, GPIO.IN)
+        GPIO.setup(self.SW3, GPIO.IN)
+        GPIO.setup(self.SW4, GPIO.IN)
+        GPIO.setup(self.SW5, GPIO.IN)
 
-        log.debug("GPIO: BCM")
-        log.debug(f"Relay 1: {relay1}")
-        log.debug(f"Relay 2: {relay2}")
-        log.debug(f"Top Switch: {top_switch}")
-        log.debug(f"Bottom Switch: {bottom_switch}")
-        log.debug(f"Sensor: {sensor}")
-        log.debug(f"Max Travel: {max_travel_time} seconds")
+    def move(self, opt):
+        log.info("[Operation Start]")
 
-        self.in_motion = False
-
-    def status(self):
-        if GPIO.input(self.SENSOR):  # If sensor is blocked
-            status = 'blocked'
-        elif self.in_motion:  # If the door is moving
-            status = 'moving'
+        if opt == 1:
+            self.motion = 1
+        elif opt == 2:
+            self.motion = 2
         else:
-            if GPIO.input(self.TOP_SWITCH) is True and GPIO.input(self.BOTTOM_SWITCH) is False:
-                status = 'up'
-            elif GPIO.input(self.TOP_SWITCH) is False and GPIO.input(self.BOTTOM_SWITCH) is True:
-                status = 'down'
+            return
+
+        start = time.time()
+        while time.time() < start + self.max_travel:
+            if GPIO.input(self.SW3) == 1:
+                GPIO.output(self.RELAY1, True)
+                GPIO.output(self.RELAY2, True)
+                self.status = 'blocked'
+            elif self.motion == 1 and GPIO.input(self.SW1) == 0 and GPIO.input(self.SW3) == 0:
+                GPIO.output(self.RELAY1, False)
+                GPIO.output(self.RELAY2, True)
+                self.status = 'closing'
+            elif self.motion == 2 and GPIO.input(self.SW2) == 0 and GPIO.input(self.SW3) == 0:
+                GPIO.output(self.RELAY1, True)
+                GPIO.output(self.RELAY2, False)
+                self.status = 'opening'
             else:
-                status = 'unknown'
+                break
+        self.motion = 0
+        GPIO.output(self.RELAY1, True)
+        GPIO.output(self.RELAY2, True)
 
-        return status
+        if GPIO.input(self.SW1) and not GPIO.input(self.SW2):
+            self.status = 'closed'
+        elif not GPIO.input(self.SW1) and GPIO.input(self.SW2):
+            self.status = 'open'
 
-    def move(self, direction):
-        exceeded_limit = True
-        door_blocked = False
-        already_at_position = False
-
-        log.info("Movement Operation Startup...")
-        log.debug(f"Requested Direction [{direction}]")
-
-        if direction == 'up':
-            if GPIO.input(self.TOP_SWITCH) is not True:  # If limit switch is not already triggered
-                log.debug(f"Relay 2 ON: pin-{self.RELAY2}")
-                GPIO.output(self.RELAY2, GPIO.LOW)
-                log.info(f"In Motion [{direction}]")
-
-                self.in_motion = True
-                start = time.time()
-                while time.time() < start + self.max_travel_time:  # Stops after at least max_travel_time
-                    if GPIO.input(self.TOP_SWITCH) is True:  # If limit switch is triggered
-                        exceeded_limit = False
-                        break
-                    else:
-                        continue
-
-                log.debug(f"Relay 2 OFF: pin-{self.RELAY2}")
-                GPIO.output(self.RELAY2, GPIO.HIGH)
-                log.info(f"Halted Motion [{direction}]")
-                self.in_motion = False
-            else:
-                already_at_position = True
-        elif direction == 'down':
-            if GPIO.input(self.SENSOR) is not True:  # If not blocked
-                if GPIO.input(self.BOTTOM_SWITCH) is not True:  # If limit switch is not already triggered
-                    log.debug(f"Relay 1 ON: pin-{self.RELAY1}")
-                    GPIO.output(self.RELAY1, GPIO.LOW)
-                    log.info(f"In Motion [{direction}]")
-
-                    self.in_motion = True
-                    start = time.time()
-                    while time.time() < start + self.max_travel_time:  # Stop least max_travel_time (Doesn't force stop)
-                        if GPIO.input(self.BOTTOM_SWITCH) is True:  # If limit switch is triggered
-                            exceeded_limit = False
-                            break
-                        elif GPIO.input(self.SENSOR) is True:
-                            door_blocked = True
-                            break
-                        else:
-                            continue
-
-                    log.debug(f"Relay 1 OFF: pin-{self.RELAY1}")
-                    GPIO.output(self.RELAY1, GPIO.HIGH)
-                    log.info(f"Halted Motion [{direction}]")
-                    self.in_motion = False
-                else:
-                    already_at_position = True
-            else:
-                door_blocked = True
-        else:
-            log.error("Invalid Direction")
-
-        if door_blocked:
-            self.move('up')
-            log.warning("Door is Blocked")
-        elif already_at_position:
-            log.warning("Door already at requested position")
-        elif exceeded_limit:  # If movement took longer than set seconds
-            log.critical(f'Exceeded movement limit of {self.max_travel_time} seconds: [{direction}]')
-        else:
-            log.info(f'Door Moved Successfully: [{direction}]')  # If door hit limit switch within allowed time
+        log.info(f'Door {self.status}')
+        log.info("[Operation Stop]")
