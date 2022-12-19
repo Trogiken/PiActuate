bf690727cbd6b22fdb5fdacfad6d2fb95f425cbd
Trogiken <noah@blaszak.com>
2022-12-18 19:24:02 -0500

    Start Webapp and connect Backend code, Fixed Logging _Initialization() stops when exception occurs

--- app.py
+++ app.py
@@ -7,14 +7,13 @@ import ast
 import logging.config
 import anvil
 import toml
-from pathlib import Path
+from time import sleep
 
 
 class _Initialization:
     def __init__(self):
         os.chdir(os.path.dirname(__file__))
-        _cwd = os.getcwd()
-        self.home = str(Path(_cwd).parents[0])  # DEBUG According to final level change '0'
+        self.home = os.getcwd()
         self.source = os.path.join(self.home, 'source')
         self.app_config_path = os.path.join(self.home, 'appConfig.conf')  # DEBUG
 
@@ -56,14 +55,18 @@ class _Initialization:
     def _is_rpi():  # DEBUG Method of identifying OS is untested
         """Check if system is running on an RPI"""
         import io
+
+        is_rpi = True
+
         try:
             with io.open('/sys/firmware/devicetree/base/model', 'r') as model:
-                if 'raspberry pi' in model.read().lower():
-                    return True
-                else:
-                    return False
+                if 'raspberry pi' not in model.read().lower():
+                    is_rpi = False
         except IOError:
-            return False
+            is_rpi = False
+
+        if not is_rpi:
+            raise OSError("System is not recognized as RPI")
 
     def _app_config_load(self):
         """Validate and return config data"""
@@ -96,7 +99,7 @@ class _Initialization:
             self.log.info("Config Loaded")
             self.log.debug(f"Loaded Config Data: {self.app_config}")
         except BaseException:
-            self.log.exception('Problem Reading AppConfig!')
+            raise AttributeError("Failed to load config data")
 
         try:  # Create Save Object
             self.log.info('Creating Save Object')
@@ -105,7 +108,7 @@ class _Initialization:
             self.save = Save(os.path.join(self.source, 'DATA.pkl'))
             self.log.info("Save object created")
         except BaseException:
-            self.log.exception('Problem Creating Save Object!')
+            raise AttributeError("Problem Creating Save Object")
 
         try:  # Read Save Data
             self.log.info('Reading Save Data')
@@ -114,7 +117,7 @@ class _Initialization:
             self.log.info("Save Loaded")
             self.log.debug(f"Loaded Save Data: {save_data}")
         except BaseException:
-            self.log.exception('Problem Reading Save Data!')
+            raise AttributeError("Problem Reading Save Data")
 
         try:  # Create Door Object
             self.log.info('Creating Door Object')
@@ -124,7 +127,7 @@ class _Initialization:
                              sw3=io['switch3'], sw4=io['switch4'], sw5=io['switch5'], travel_time=prop['travel_time'])
             self.log.info("Door object created")
         except BaseException:
-            self.log.exception('Problem Creating Door Object!')
+            raise AttributeError("Problem Creating Door Object")
 
         try:  # Create Auto Object
             self.log.info('Creating Auto Object')
@@ -136,7 +139,7 @@ class _Initialization:
                              sunset_offset=int(save_data['sunset_offset']))
             self.log.info("Automation object created")
         except BaseException:
-            self.log.exception('Problem Creating Auto Object!')
+            raise AttributeError("Problem Creating Auto Object")
 
         self.log.debug('Executing Saved States...')
         if save_data['automation']:
@@ -148,15 +151,22 @@ class _Initialization:
         else:
             self.log.debug('Nothing Executed!')
 
-    def run(self):
+    def start(self):
         """Run Tests"""
         self._logging_config_load()
         self.log.info("Startup...")
 
-        if self._is_rpi():
+        try:
+            self._is_rpi()
+        except Exception:
+            self.log.exception("Program is designed for RPI only!")
+            raise
+
+        try:
             self.load_objects()
-        else:
-            self.log.exception(OSError('Program is designed for RPI only!'))  # DEBUG Result Unknown
+        except Exception:
+            self.log.exception("Error loading objects")
+            raise
 
         self.log.info("Startup Complete!")
 
@@ -165,119 +175,145 @@ class App(_Initialization):
     def __init__(self):
         super().__init__()
         init = _Initialization()
-        init.run()
+        init.start()
 
-    def run(self):  # DEBUG Might call _Initialization.run() instead
-        # TODO Have this run anvil runtime command with properties form app_config
-        pass
+    @staticmethod
+    def connect(ipv4, port, key):  # DEBUG Untested
+        anvil.server.connect(key, url=f"ws://{ipv4}:{port}/_/uplink")
 
-    # TODO Connect to uplink with key
-    # TODO Make anvil callable
-    # TODO Link shutdown program button from webapp
-    def run_auto(self):
-        """Calls auto.run()"""
-        self.log.debug("CALLED")
-        self.auto.run()
+        # TODO Link shutdown program button from webapp
+        @anvil.server.callable()
+        def run_auto(self):
+            """Calls auto.run()"""
+            self.log.debug("CALLED")
+            self.auto.run()
 
-    def stop_auto(self):
-        """Calls auto.stop()"""
-        self.log.debug("CALLED")
-        self.auto.stop()
+        @anvil.server.callable()
+        def stop_auto(self):
+            """Calls auto.stop()"""
+            self.log.debug("CALLED")
+            self.auto.stop()
 
-    def run_aux(self):
-        """Calls door.run_aux()"""
-        self.log.debug("CALLED")
-        self.door.run_aux()
+        @anvil.server.callable()
+        def run_aux(self):
+            """Calls door.run_aux()"""
+            self.log.debug("CALLED")
+            self.door.run_aux()
 
-    def stop_aux(self):
-        """Calls door.stop_aux()"""
-        self.log.debug("CALLED")
-        self.door.stop_aux()
+        @anvil.server.callable()
+        def stop_aux(self):
+            """Calls door.stop_aux()"""
+            self.log.debug("CALLED")
+            self.door.stop_aux()
 
-    def change_rise(self, offset):
-        """Calls auto.set_sunrise(offset), saves the new value"""
-        self.log.debug("CALLED")
-        self.auto.set_sunrise(offset)
-        self.save.change('sunrise_offset', offset)
+        @anvil.server.callable()
+        def change_rise(self, offset):
+            """Calls auto.set_sunrise(offset), saves the new value"""
+            self.log.debug("CALLED")
+            self.auto.set_sunrise(offset)
+            self.save.change('sunrise_offset', offset)
 
-    def change_set(self, offset):
-        """Calls auto.set_sunset(offset), saves the new value"""
-        self.log.debug("CALLED")
-        self.auto.set_sunset(offset)
-        self.save.change('sunset_offset', offset)
+        @anvil.server.callable()
+        def change_set(self, offset):
+            """Calls auto.set_sunset(offset), saves the new value"""
+            self.log.debug("CALLED")
+            self.auto.set_sunset(offset)
+            self.save.change('sunset_offset', offset)
 
-    def refresh_auto(self):
-        """Calls auto.refresh()"""
-        self.log.debug("CALLED")
-        self.auto.refresh()
+        @anvil.server.callable()
+        def refresh_auto(self):
+            """Calls auto.refresh()"""
+            self.log.debug("CALLED")
+            self.auto.refresh()
 
-    def get_times(self):
-        """Returns sunrise and sunset times in a dictionary"""
-        self.log.debug("CALLED")
-        return {'sunrise': self.auto.active_sunrise(),
-                'sunset': self.auto.active_sunset()}
+        @anvil.server.callable()
+        def get_times(self):
+            """Returns sunrise and sunset times in a dictionary"""
+            self.log.debug("CALLED")
+            return {'sunrise': self.auto.active_sunrise(),
+                    'sunset': self.auto.active_sunset()}
 
-    def c_state(self, variable=None):
-        """
-        Read save file
+        @anvil.server.callable()
+        def c_state(self, variable=None):
+            """
+            Read save file
 
-            Parameters:
-                variable (str), optional: key in dictionary
+                Parameters:
+                    variable (str), optional: key in dictionary
 
-            Returns:
-                save.load() (dict): all data
-                save.load()[variable] (str, int, float, bool): value of specified key
-        """
-        self.log.debug("CALLED")
-        if variable is not None:
-            return self.save.load()[variable]
-        else:
-            return self.save.load()
+                Returns:
+                    save.load() (dict): all data
+                    save.load()[variable] (str, int, float, bool): value of specified key
+            """
+            self.log.debug("CALLED")
+            if variable is not None:
+                return self.save.load()[variable]
+            else:
+                return self.save.load()
 
-    def rpi_status(self):
-        """Called by WebApp to check if it still has connection to this program, Returns None"""
-        # DEBUG How would this work locally
-        return
-
-    def door_status(self):
-        """Returns func call door.get_status()"""
-        return self.door.get_status()
-
-    def reset_config(self):
-        """"Calls save.reset()"""
-        self.save.reset()
-        return
-
-    def shutdown(self, parm='h'):
-        """
-        Shutdown or Restart system
-
-        If parm is changed to 'r' the system will restart
-
-            Parameters:
-                parm (str), optional: Shutdown flag
-        """
-        if parm == 'h':
-            self.log.warning("Shutting Down...")
-        elif parm == 'r':
-            self.log.warning("Restarting...")
-        else:
+        @anvil.server.callable()
+        def rpi_status(self):
+            """Called by WebApp to check if it still has connection to this program, Returns None"""
+            # DEBUG How would this work locally
             return
 
-        self.stop_aux()
-        self.stop_auto()
-        self.door.cleanup()
-        os.system(f'sudo shutdown -{parm} now')
+        @anvil.server.callable()
+        def door_status(self):
+            """Returns func call door.get_status()"""
+            return self.door.get_status()
 
-    def move(self, opt):
-        """Takes opt (1 or 2) and calls door.move(opt)"""
-        self.log.debug("CALLED")
-        self.door.move(opt)
+        @anvil.server.callable()
+        def reset_config(self):
+            """"Calls save.reset()"""
+            self.save.reset()
+            return
 
-    def change(self, variable, value):
-        """Calls save.change(variable, value)"""
-        self.log.debug("CALLED")
-        self.save.change(variable, value)
+        @anvil.server.callable()
+        def shutdown(self, parm='h'):
+            """
+            Shutdown or Restart system
+
+            If parm is changed to 'r' the system will restart
+
+                Parameters:
+                    parm (str), optional: Shutdown flag
+            """
+            if parm == 'h':
+                self.log.warning("Shutting Down...")
+            elif parm == 'r':
+                self.log.warning("Restarting...")
+            else:
+                return
+
+            self.stop_aux()
+            self.stop_auto()
+            self.door.cleanup()
+            os.system(f'sudo shutdown -{parm} now')
+
+        @anvil.server.callable()
+        def move(self, opt):
+            """Takes opt (1 or 2) and calls door.move(opt)"""
+            self.log.debug("CALLED")
+            self.door.move(opt)
+
+        @anvil.server.callable()
+        def change(self, variable, value):
+            """Calls save.change(variable, value)"""
+            self.log.debug("CALLED")
+            self.save.change(variable, value)
+
+        anvil.server.wait_forever()
+
+    def run(self):
+        net = self.app_config['network']
+
+        ipv4 = net['ipv4']
+        port = net['port']
+        key = net['key']
+
+        os.system(f"anvil-app-server --app Door_Control --origin http://{ipv4}:{port}/ --uplink-key={key}")
+        sleep(10)
+        self.connect(ipv4, port, key)
 
 
 if __name__ == '__main__':
