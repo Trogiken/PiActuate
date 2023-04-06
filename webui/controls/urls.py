b9701b88b65e4a33ff7a662981680c8ca8998e87
Trogiken <noah@blaszak.com>
2023-04-05 22:57:27 -0400

    Basic login structure configured Superuser created: Admin, doorcontrol123

---
+++ webui/controls/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.RedirectToControlsView.as_view(), name="redirect-to-controls-page"),
+    path("controls/", views.ControlsView.as_view(), name="controls-page"),
+]
