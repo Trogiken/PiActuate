4567db170add1a53ffcb3da1e6f75b388dece92a
Trogiken <noah@blaszak.com>
2023-04-08 17:55:34 -0400

    SystemConfig page styled, 2 issues Arrow svgs are weird, help messages are not styled

---
+++ webui/controls/static/controls/systemconfig-help-hide.js
@@ -0,0 +1,8 @@
+function toggleHelp(a) {
+    var x = document.getElementById(a);
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+  }
+}
