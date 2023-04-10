05a2a3ae5d97e9bf076ced8f3d99116926616a86
Trogiken <noah@blaszak.com>
2023-04-10 00:30:49 -0400

    Overhall 1 todo Format the help texts

--- webui/controls/static/controls/systemconfig-help-hide.js
+++ webui/controls/static/controls/systemconfig-help-hide.js
@@ -1,8 +1,15 @@
+window.onload = function() {
+    var x = document.getElementsByClassName("help-text");
+    for (var i = 0; i < x.length; i++) {
+        x[i].style = "display: none;";
+    }
+}
+
 function toggleHelp(a) {
     var x = document.getElementById(a);
-    if (x.style.display === "inline-block") {
+    if (x.style.display === "block") {
         x.style = "display: none;"
     } else {
-        x.style = "display: inline-block;"
+        x.style = "display: block;"
   }
 }
