ad04443290ad6ce70bd274145b85e764ab53fcdd
noah <70231827+Trogiken@users.noreply.github.com>
2023-04-12 20:51:54 -0400

    Cleanly hide help text on load without .js

--- webui/controls/static/controls/systemconfig-help-hide.js
+++ webui/controls/static/controls/systemconfig-help-hide.js
@@ -1,15 +1,9 @@
-window.onload = function() {
-    var x = document.getElementsByClassName("help-text");
-    for (var i = 0; i < x.length; i++) {
-        x[i].style = "display: none;";
-    }
-}
-
 function toggleHelp(a) {
+    // Toggle help text
     var x = document.getElementById(a);
     if (x.style.display === "block") {
         x.style = "display: none;"
     } else {
         x.style = "display: block;"
-  }
+    }
 }
