db4e77dfbaf4c9117d164dac351e54d61db4886b
Trogiken <70231827+Trogiken@users.noreply.github.com>
2023-05-06 14:03:47 -0400

    Remove_pagerefresh_updown_controls (#26)

--- webui/controls/consumers.py
+++ webui/controls/consumers.py
@@ -1,8 +1,10 @@
 import json
 
 from channels.generic.websocket import WebsocketConsumer
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class DashboardConsumer(WebsocketConsumer):
+
+class DashboardConsumer(LoginRequiredMixin, WebsocketConsumer):
     def connect(self):
         self.accept()
 
@@ -14,6 +16,39 @@ class DashboardConsumer(WebsocketConsumer):
         if data.get('message') == 'get_status':
             from controls.views import runtime
             self.send(text_data=json.dumps({
+                "signal": "200",
                 "message": runtime.door.status
                 })
             ) # send door status
+
+
+class DoorMovementConsumer(LoginRequiredMixin, WebsocketConsumer):
+    def connect(self):
+        self.accept()
+    
+    def disconnect(self, close_code):
+        pass
+
+    def receive(self, text_data=None, bytes_data=None):
+        from controls.views import runtime
+        data = json.loads(text_data)
+        if data.get('message') == 'open':
+            runtime.door.move(2)
+            self.send(text_data=json.dumps({
+                "signal": "200",
+                "message": "Door is opening"
+                })
+            )
+        elif data.get('message') == 'close':
+            runtime.door.move(1)
+            self.send(text_data=json.dumps({
+                "signal": "200",
+                "message": "Door is closing"
+                })
+            )
+        else:
+            self.send(text_data=json.dumps({
+                "signal": "400",
+                "message": "Invalid request"
+                })
+            )
---
+++ webui/controls/static/controls/engine-ops.js
@@ -0,0 +1,56 @@
+const statusSocket = new WebSocket(
+    'ws://'
+    + window.location.host
+    + '/ws/dashboard/status/'
+  );
+document.addEventListener('DOMContentLoaded', function() {
+    window.setInterval(getStatus, 1000);
+  });
+function getStatus() {
+    statusSocket.send(JSON.stringify({
+        'message': 'get_status'
+    }));
+}
+statusSocket.onmessage = function(e) {
+    const data = JSON.parse(e.data);
+    document.querySelector('#status_text').innerHTML = data.message;
+};
+
+
+const movementSocket = new WebSocket(
+  'ws://'
+  + window.location.host
+  + '/ws/door/movement/'
+);
+// function that accepts a variable that sends 'open' or 'close' to the server. Also show loading animation while waiting for response.
+function sendDoorCommand(command) {
+  showLoading();
+  movementSocket.send(JSON.stringify({
+      'message': command
+  }));
+}
+// on message from server, if response is 200, hide loading animation, if response is 400, show error message.
+movementSocket.onmessage = function(e) {
+  const data = JSON.parse(e.data);
+  if (data.signal === '200') {
+      document.querySelector('#loading-text').innerHTML = 'Success: ' + data.message;
+      setTimeout(hideLoading, 2000);
+      hideLoading();
+  } else {
+      document.querySelector('#loading-text').innerHTML = 'Error: ' + data.message;
+      setTimeout(hideLoading, 2000);
+      hideLoading();
+  }
+}
+
+
+const loadingContainer = document.getElementById('loading-container');
+const loadingText = document.getElementById('loading-text');
+function showLoading() {
+  loadingContainer.style.display = 'block';
+  loadingText.style.display = 'block';
+}
+function hideLoading() {
+  loadingContainer.style.display = 'none';
+  loadingText.style.display = 'none';
+}
