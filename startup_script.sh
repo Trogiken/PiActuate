ac548fd0f8859c4bf3b2c21dd7a1986cb551e880
Trogiken <noah@blaszak.com>
2023-05-07 12:27:53 -0400

    startup edit

--- startup_script.sh
+++ startup_script.sh
@@ -13,6 +13,36 @@ python -m pip install -r docs/requirements.txt
 sudo apt-get install -y nginx
 clear
 
+mkdir -pv config/gunicorn/
+touch config/gunicorn/gunicorn.conf.py
+echo "
+wsgi_app = 'webui.wsgi:application'
+
+loglevel = 'debug'
+
+workers = 2
+
+bind = '0.0.0.0:8000'
+
+reload = True
+
+accesslog = errorlog = '/var/log/gunicorn/dev.log'
+
+capture_output = True
+
+pidfile = '/var/run/gunicorn/dev.pid'
+
+daemon = True" > config/gunicorn/gunicorn.conf.py
+
+sudo mkdir -pv /var/{log,run}/gunicorn/
+# DEBUG sudo chown -cR ubuntu:ubuntu /var/{log,run}/gunicorn/
+
+clear
+
+cd webui
+gunicorn -c ../config/gunicorn/gunicorn.conf.py
+
+
 cd webui
 # ideally have use nginx as a proxy to filter traffic between the two
-gunicorn app.wsgi:application -b 0.0.0.0 -p 8000 --reload & daphne app.asgi:application -b 0.0.0.0 -p 8089 &
+# DEBUG gunicorn app.wsgi:application -b 0.0.0.0 -p 8000 --reload & daphne app.asgi:application -b 0.0.0.0 -p 8089 &
