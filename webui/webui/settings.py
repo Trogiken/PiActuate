40cfd873d8aa15721b9190e99db625abd632caf3
Trogiken <noah@blaszak.com>
2023-05-06 22:34:37 -0400

    secret key update

--- webui/webui/settings.py
+++ webui/webui/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 from django.core.management.utils import get_random_secret_key
+SECRET_KEY = getenv('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv('IS_DEVELOPMENT', True)
