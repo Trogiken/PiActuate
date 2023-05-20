13cae062d9f4b1bb599fdcd043e4ea4aa7fa60ea
Trogiken <noah@blaszak.com>
2023-05-20 12:48:35 -0400

    UNTESTED improved importing runtime to consumer

--- webui/controls/consumers.py
+++ webui/controls/consumers.py
@@ -4,24 +4,30 @@ from channels.generic.websocket import WebsocketConsumer
 
 
 class DoorConsumer(WebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.runtime = None
+
     def connect(self):
+        if self.runtime is None:
+            from controls.views import runtime
+            self.runtime = runtime
         self.accept()
     
     def disconnect(self, code):
         return super().disconnect(code)
     
     def receive(self, text_data=None, bytes_data=None):
-        from controls.views import runtime
         data = json.loads(text_data)
         if data.get('message') == 'get_status':
             self.send(text_data=json.dumps({
                 "signal": "200",
                 "command": data.get('message'),
-                "message": runtime.door.status
+                "message": self.runtime.door.status
                 })
             )
         elif data.get('message') == 'open':
-            runtime.door.move(2)
+            self.runtime.door.move(2)
             self.send(text_data=json.dumps({
                 "signal": "200",
                 "command": data.get('message'),
@@ -29,7 +35,7 @@ class DoorConsumer(WebsocketConsumer):
                 })
             )
         elif data.get('message') == 'close':
-            runtime.door.move(1)
+            self.runtime.door.move(1)
             self.send(text_data=json.dumps({
                 "signal": "200",
                 "command": data.get('message'),
