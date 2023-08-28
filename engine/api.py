3d603b0b6b402d559f0fe4a8a281ea115f4d71f3
noah <70231827+Trogiken@users.noreply.github.com>
2023-08-28 17:48:21 -0400

    Update requirements for API

--- engine/api.py
+++ engine/api.py
@@ -8,6 +8,8 @@ app = FastAPI()
 # Start this api
 # uvicorn main:app --host 0.0.0.0 --port 8000
 
+# TODO Configure setup_script.sh to start api server on boot
+
 # TODO POST configure: If you transmit the database object as a dict, the way runtime accesses it
 # will not work. You need to transmit the object itself. Try to use pickle to serialize
 # the object and send it over the wire. Then deserialize it on the other end.
