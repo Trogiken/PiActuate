45d610b35037921a712ae76350257cacc9d44a9c
Trogiken <70231827+Trogiken@users.noreply.github.com>
2023-10-08 14:01:21 -0400

    max pin off by 1 fix

--- webui/controls/forms.py
+++ webui/controls/forms.py
@@ -25,7 +25,7 @@ class SystemConfigForm(forms.ModelForm):
     def clean(self):
         cleaned_data = super().clean()
         board_mode = cleaned_data.get("board_mode")
-        pins = range(2, 27) if board_mode == "BCM" else [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40]
+        pins = range(2, 28) if board_mode == "BCM" else [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40]
         for i in range(1, 6):
             if cleaned_data.get(f"switch{i}") not in pins:
                 self.add_error(f"switch{i}", "Invalid Pin")
