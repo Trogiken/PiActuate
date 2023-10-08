69a924090b6846416ce1da91deb1484a8bca2027
Trogiken <70231827+Trogiken@users.noreply.github.com>
2023-10-08 13:49:15 -0400

    pin validations for bcm and board mode

--- webui/controls/forms.py
+++ webui/controls/forms.py
@@ -20,6 +20,18 @@ class SystemConfigForm(forms.ModelForm):
     class Meta:
         model = SystemConfig
         fields = ("__all__")
+    
+    # pin validation based off of board mode
+    def clean(self):
+        cleaned_data = super().clean()
+        board_mode = cleaned_data.get("board_mode")
+        pins = range(2, 27) if board_mode == "BCM" else [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40]
+        for i in range(1, 6):
+            if cleaned_data.get(f"switch{i}") not in pins:
+                self.add_error(f"switch{i}", "Invalid Pin")
+        for relay in ["relay1", "relay2"]:
+            if cleaned_data.get(relay) not in pins:
+                self.add_error(relay, "Invalid Pin")
 
 
 class UserLoginForm(AuthenticationForm):
