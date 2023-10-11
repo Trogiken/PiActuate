73ae2af0b94a0573574e5b5d0b5bf8e2b12a90ce
Trogiken <70231827+Trogiken@users.noreply.github.com>
2023-10-10 21:10:35 -0400

    ready function test

--- webui/controls/apps.py
+++ webui/controls/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+from controls.models import SystemConfig, StartupConfig
+from controls.api_comms import ApiComms
 
 
 class ControlsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'controls'
+
+    def ready(self):
+        api = ApiComms()
+        if SystemConfig.objects.exists() and StartupConfig.objects.exists():
+            api.configure()
