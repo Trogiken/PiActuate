0c56e76bb97311be8f2b97c2c20a5f025136cbec
Trogiken <70231827+Trogiken@users.noreply.github.com>
2024-05-06 16:38:35 -0400

    Update middleware position

--- webui/webui/settings.py
+++ webui/webui/settings.py
@@ -54,12 +54,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'controls.middleware.DaphneStatusMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'controls.middleware.DaphneStatusMiddleware',
 ]
 
 ROOT_URLCONF = 'webui.urls'
