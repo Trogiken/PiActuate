66142230db7664c556aa6e04c50f0c81c36a2057
Trogiken <70231827+Trogiken@users.noreply.github.com>
2024-05-06 15:50:22 -0400

    remove logging from update script

--- update.py
+++ update.py
@@ -14,31 +14,14 @@ if not os.access(setup_shell_path, os.X_OK):
     print('Shell script is not executable.')
     exit(1)
 
-
-def get_package_location(package_name):
-    try:
-        dist = pkg_resources.get_distribution(package_name)
-        return dist.location
-    except pkg_resources.DistributionNotFound:
-        return None
-
-
-def write_update_log(msg: str):
-    with open(os.path.join(main_dir, 'update.log'), 'w') as f:
-        f.write(f"""More specifc update logs are stored in '{get_package_location("pyupgrader")}/pyupgrader'\n""")
-        f.write(msg)
-
-
 if __name__ == '__main__':
     command = ["sudo", "/bin/bash", setup_shell_path]
 
     try:
         result = subprocess.run(command, check=True, capture_output=True, text=True)
-        write_update_log(result.stdout)
         print('Shell script finished running, rebooting...')
         os.system("sudo reboot")
     except subprocess.CalledProcessError as e:
         print('Shell script did not finish successfully.')
         print('Output:', e.output)
         print('Error:', e.stderr)
-        write_update_log(e.stderr)
