74853900fd62cae31a6cd737ac87a1d1cc519f2e
Trogiken <70231827+Trogiken@users.noreply.github.com>
2024-05-06 16:20:48 -0400

    Prevent login when updating

---
+++ webui/controls/middleware.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponseServerError
+from django.urls import reverse_lazy
+import subprocess
+
+
+class DaphneStatusMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.login_url = reverse_lazy('login')
+
+    def __call__(self, request):
+        if request.method == 'POST' and not self.is_daphne_running() and request.path == self.login_url:
+            return HttpResponseServerError('Daphne service is not running. Please try again later.')
+        return self.get_response(request)
+    
+    def is_daphne_running(self.):
+        try:
+            subprocess.run(['systemctl', 'is-active', '--quiet', 'daphne'], check=True)
+            return True
+        except subprocess.CalledProcessError:
+            return False
--- webui/webui/settings.py
+++ webui/webui/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'controls.middleware.DaphneStatusMiddleware',
 ]
 
 ROOT_URLCONF = 'webui.urls'
