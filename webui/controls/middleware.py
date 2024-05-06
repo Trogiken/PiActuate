038630523b5e6d66a3862fee5b66ff99511a92de
Trogiken <70231827+Trogiken@users.noreply.github.com>
2024-05-06 18:25:27 -0400

    Update server side message

--- webui/controls/middleware.py
+++ webui/controls/middleware.py
@@ -1,14 +1,18 @@
+from django.http import HttpResponseServerError
+from django.shortcuts import render
+from pathlib import Path
 import os
 
 
 class UpdateStatusMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        self.main_dir = os.path.dirname(os.path.abspath(__file__))
+        self.main_dir = str(Path(__file__).resolve().parents[2])
         self.lock_file = os.path.join(self.main_dir, 'update.lock')
 
     def __call__(self, request):
         # check daphne only because thats the process we use to run the update script
         if request.method == 'POST' and os.path.exists(self.lock_file):
-            raise Exception("Update in progress, please wait.")
+            context = {'update_message': 'Update in progress, check back later'}
+            return HttpResponseServerError(render(request, '403.html', context))
         return self.get_response(request)
